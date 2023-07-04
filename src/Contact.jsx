ec09c1187ff05fda74010171e6b4bc9fa919a9ea
Sahilm416 <114013781+Sahilm416@users.noreply.github.com>
2023-07-04 13:19:08 +0530

    added links to contact

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,25 +1,34 @@
-import Git from './images/github.png'
-import Linked from './images/linkedin.png'
-import Mail from './images/gmail.png'
-import './App.css'
+import Git from "./images/github.png";
+import Linked from "./images/linkedin.png";
+import Mail from "./images/gmail.png";
+import "./App.css";
 
-export default function Contact (){
-    return (
-        <>
-         <div className='whole-contact'>
-         <div className='main-contact'>
-           <div>
-           <a href=""><img src={Git} alt="" /><h3>gitHub</h3></a>
-           </div>
-           <div>
-           <a href=""><img src={Linked} alt="" /> <h3>Linkedin</h3></a> 
-           </div>
-           <div>
-           <a href=""><img src={Mail} alt="" /><h3>G-mail</h3></a>
-           </div>
-         </div>
-         </div>
-         
-        </>
-    )
+export default function Contact() {
+  return (
+    <>
+      <div className="whole-contact">
+        <div className="main-contact">
+          <div>
+            <a href="https://github.com/Sahilm416">
+              <img src={Git} alt="" />
+              <h3>gitHub</h3>
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://www.linkedin.com/in/sahil-mulani501/"
+            >
+              <img src={Linked} alt="" /> <h3>Linkedin</h3>
+            </a>
+          </div>
+          <div>
+            <a href="mailto:sahilmulani501@gmail.com">
+              <img src={Mail} alt="" />
+              <h3>G-mail</h3>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
