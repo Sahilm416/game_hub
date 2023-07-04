37a35869a38083912b97d9a411302b8b2b1fafc2
Sahilm416 <114013781+Sahilm416@users.noreply.github.com>
2023-07-04 14:23:07 +0530

    fixed clip

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Nav from './Nav'
-import Home from './Home'
+
 import About from './About'
 import Contact from './Contact'
 import Loader from './loader'
@@ -14,6 +14,7 @@ import SmoothScrollbar from 'smooth-scrollbar'
 
 
 const Games = lazy(()=> import('./Games'));
+const Home = lazy(()=> import('./Home'));
 function App() {
  
 
